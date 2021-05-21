cc5438aeb728c6cb11474507435a8c60269bdbb1
Cal Corbin <calcorbin@Cals-MacBook-Pro.local>
2021-05-21 16:52:39 -0500

    Allow user to select action

--- constants.js
+++ constants.js
@@ -9,9 +9,17 @@ const coolCactiArt = `
     |  |
 `;
 
-const introMessage = `Welcome to the Cacti Corral!!!
+const introMessage = `
+  Welcome to the Cacti Corral!!!
   You will have 7 turns, each turn representing a week, to raise your cactus.
   Your goal is to either get your cactus to 10 inches tall OR to get it to flower.
-  There is also the possibility your cactus will dry and up die, so try to avoid that.`;
+  There is also the possibility your cactus will dry and up die, so try to avoid that.
+  `;
 
-module.exports = { coolCactiArt, introMessage };
+const availableActions = [
+  'Water the cacti',
+  'Turn on the sun lamp',
+  'Feed it a fertilizer pill',
+];
+
+module.exports = { coolCactiArt, introMessage, availableActions };
--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
-const { prompt, Select } = require('enquirer');
-const { coolCactiArt, introMessage } = require('./constants');
+const { Select } = require('enquirer');
+const { coolCactiArt, introMessage, availableActions } = require('./constants');
 
 async function gameIntro(cactus) {
   if (cactus.weeksOld > 0) {
@@ -9,13 +9,59 @@ async function gameIntro(cactus) {
   console.log(introMessage);
 }
 
+async function pourWater(cactus) {
+  console.log('watering cactus');
+}
+
+async function turnOnSunLamp(cactus) {
+  console.log('turning on the sun lamp');
+}
+
+async function addFertilizer(cactus) {
+  console.log('fertilizing the cactus');
+}
+
 async function startRound(cactus) {
   if (cactus.weeksOld > 7) {
     // We want to end the game if cactus is older than 7 weeks.
     return;
   }
 
-  console.log(cactus);
+  const actions = {
+    water: availableActions[0],
+    sunlight: availableActions[1],
+    fertilize: availableActions[2],
+  };
+
+  let actionsUsed = 0;
+
+  const userPrompt = actions === 0 ? '\nWhat would you like to do to the cactus first?'
+    : 'How would you like to use your last action this week?';
+
+  const question = {
+    name: 'selectAction',
+    message: userPrompt,
+    choices: availableActions,
+  };
+
+  const round = new Select(question);
+
+  await round.run()
+    .then(async (answer) => {
+      actionsUsed += 1;
+
+      if (answer === actions.water) {
+        pourWater(cactus);
+      }
+
+      if (answer === actions.sunlight) {
+        turnOnSunLamp(cactus);
+      }
+
+      if (answer === actions.fertilize) {
+        addFertilizer(cactus);
+      }
+    });
 }
 
 async function runGame(cactus) {
