3c06befcbccd735338d33234f0e3e7b0f24730bc
Cal Corbin <calcorbin@Cals-MacBook-Pro.local>
2021-05-21 16:15:19 -0500

    Create constants file

---
+++ constants.js
@@ -0,0 +1,17 @@
+const coolCactiArt = `
+    ,*-.
+    |  |
+,.  |  |
+| |_|  | ,.
+\`---.  |_| |
+    |  .--\`
+    |  |
+    |  |
+`;
+
+const introMessage = `Welcome to the Cacti Corral!!!
+  You will have 7 turns, each turn representing a week, to raise your cactus.
+  Your goal is to either get your cactus to 10 inches tall OR to get it to flower.
+  There is also the possibility your cactus will dry and up die, so try to avoid that.`;
+
+module.exports = { coolCactiArt, introMessage };
--- controller.js
+++ controller.js
@@ -1,7 +1,25 @@
 const { prompt, Select } = require('enquirer');
+const { coolCactiArt, introMessage } = require('./constants');
 
-async function runGame(cactus) {
+async function gameIntro(cactus) {
+  if (cactus.weeksOld > 0) {
+    return;
+  }
+  console.log(coolCactiArt);
+  console.log(introMessage);
+}
+
+async function startRound(cactus) {
+  if (cactus.weeksOld > 7) {
+    return;
+  }
+
   console.log(cactus);
+}
+async function runGame(cactus) {
+  await gameIntro(cactus);
+
+  await startRound(cactus);
 }
 
 module.exports = { runGame };
