a7ae8aa04b0b94e241ebe0d4aadb4ba145aa4b3b
Cal Corbin <calcorbin@Cals-MacBook-Pro.local>
2021-05-21 21:22:50 -0500

    Fix typo

--- constants.js
+++ constants.js
@@ -39,7 +39,7 @@ const introMessage = `
   Welcome to the Cacti Corral!!!
   You will have 7 turns, each turn representing a week, to raise your cactus.
   Your goal is to either get your cactus to 10 inches tall OR to get it to flower.
-  There is also the possibility your cactus will dry and up die, so try to avoid that.
+  There is also the possibility your cactus will dry up and die, so try to avoid that.
   `;
 
 const availableActions = [
